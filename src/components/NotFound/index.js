5989848a716d0170b4d3bea96d77b5e6119f69c6
Aarif-khan-mohammad <142022266+Aarif-khan-mohammad@users.noreply.github.com>
2024-03-29 03:27:13 +0530

    Update index.js

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './index.css'
 
 const NotFound = () => {
   return (
-    <div>
-      <h1 className="text-center">Page not found</h1>
+    <div className='notfound'>
+      <h1 className="text-center">Page Not Found</h1>
     </div>
   )
 }
